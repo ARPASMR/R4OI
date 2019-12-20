8f65dc82488bf85cff8dfb7c776c90ffa1f91edc
msalvati <m.salvati@arpalombardia.it>
2019-12-20 12:20:00 +0100

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,15 +1,17 @@
+
 #!/bin/bash
-numsec=3600 # 1 h 
+
+numsec=3600 # 1 h
 sleep $numsec
 while [ 1 ]
 do
   if [ $SECONDS -ge $numsec ]
   then
-    ora=$(date --date "1 hours ago" +%Y%m%d%H)
+    ora=$(date --date "3 hours ago" +%Y%m%d%H)
     ./datiGRADS.R $ora
     ./t2m19 $ora 1 datiGRADS.dat 3 2 ./ ./
-    # inserire interpolazione rhtd
-    # inserire interpolazione pluv
+    ./rhtd19 $ora 1 datiGRADS.dat 3 3 2 ./ ${ora:0:8}t2m_s.dat ${ora:0:8}t2m_g.dat ./
+    ./plzln19 $ora 1 datiGRADS.dat 3 1 2 ./ ${ora:0:8}t2m_s.dat ./
     # inserire upload rasdaman
     # inserire update DBMETEO
     SECONDS=0
