08eb5d58ff073514252ca57948e70a13deaacbc0
msalvati <m.salvati@arpalombardia.it>
2020-02-07 12:29:38 +0100

    1.lettura funzione s3put  2.upload delle analisi su MINIO

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+source s3put.sh 
 numsec=3600 # 1 h 
 sleep $numsec
 while [ 1 ]
@@ -11,7 +11,16 @@ do
     ./t2m19 $ora 1 datiGRADS.dat 3 2 ./ ./
     ./rhtd19 $ora 1 datiGRADS.dat 3 3 2 ./ ${ora:0:8}t2m_s.dat ${ora:0:8}t2m_g.dat ./
     ./plzln19 $ora 1 datiGRADS.dat 3 1 2 ./ ${ora:0:8}t2m_s.dat ./
-    # inserire upload rasdaman
+    # upload in minio
+    for dati in $ora_*.txt ; do 
+      putS3 $dati "/" "analisi"
+      if [[ "$?" != "0" ]] then
+        echo "Errore nel caricare su MINIO il file " $dati
+      else 
+        "Caricato su MINIO il file " $dati
+        rm -v $dati
+      fi
+    done
     # inserire update DBMETEO
     SECONDS=0
     sleep $numsec
