9f60549343ed5f03d577130fdbf63026e0573f1b
msalvati <m.salvati@arpalombardia.it>
2020-03-25 17:36:41 +0100

    corretto nome file ascii

--- t2m19.f90
+++ t2m19.f90
@@ -512,8 +512,8 @@ program t2m19
   write (6,*) ' '
   call flush(6)
 ! nome file per output ascii
-  FULFIASCIIa = OUTDIR(1:lodir) // datehr(n)(1:10) //'_32632_32632_32632_t2m_ana.txt'
-  FULFIASCIIb = OUTDIR(1:lodir) // datehr(n)(1:10) //'_32632_32632_t2m_bkg.txt'
+  FULFIASCIIa = OUTDIR(1:lodir) // datehr(n)(1:10) //'_32632_t2m_ana.txt'
+  FULFIASCIIb = OUTDIR(1:lodir) // datehr(n)(1:10) //'_32632_t2m_bkg.txt'
   FULFIASCIIi = OUTDIR(1:lodir) // datehr(n)(1:10) //'_32632_t2m_idi.txt'
   write (6,'(A)') 'Nomi file ascii: ',FULFIASCIIa,FULFIASCIIb,FULFIASCIIi
   !write (6,'(A)') ' ok?'
