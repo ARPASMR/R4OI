ce9b96f4048f59f0a13de7cada76e57911a89c6e
msalvati <m.salvati@arpalombardia.it>
2020-04-16 10:52:31 +0200

    + f90 log, cambiato $dati in upload su minio

--- launcher.sh
+++ launcher.sh
@@ -9,13 +9,18 @@ do
     ora=$(date --date "2 hours ago" +%Y%m%d%H)
     echo "Chiedo i dati per "$ora
     ./datiGRADS.R $ora
-    ./t2m19 $ora 1 datiGRADS.dat 3 2 ./ ./
-    ./rhtd19 $ora 1 datiGRADS.dat 3 3 2 ./ ${ora:0:8}t2m_s.dat ${ora:0:8}t2m_g.dat ./
-    ./plzln19 $ora 1 datiGRADS.dat 3 1 2 ./ ${ora:0:8}t2m_s.dat ./
+    ./t2m19 $ora 1 datiGRADS.dat 3 2 ./ ./  > t2m.log
+    ./rhtd19 $ora 1 datiGRADS.dat 3 3 2 ./ ${ora:0:8}t2m_s.dat ${ora:0:8}t2m_g.dat ./  > rhtd.log
+    ./plzln19 $ora 1 datiGRADS.dat 3 1 2 ./ ${ora:0:8}t2m_s.dat ./ > plzln.log
     echo "Finito interpolazione per ora: "$ora
+    echo "########"
+    echo "File presenti:  "
+    ls -1 *
+    echo "########"
     echo "Upload su minio..."
     # upload in minio
-    for dati in $ora_*.txt ; do 
+    for dati in *_32632_$ora.txt ; do
+      echo "file per upload:  "$dati 
       putS3 "." $dati "" "analisi"
       if [[ "$?" != "0" ]] 
       then
@@ -27,6 +32,12 @@ do
       rm -v temperatura_*.csv
       rm -v pluviometrizln_*.csv
       rm -v umidita_*.csv
+      rm ${ora:0:8}t2m_s.dat
+      rm ${ora:0:8}t2m_g.dat
+      rm ${ora:0:8}tdrh_s.dat
+      rm ${ora:0:8}tdrh_g.dat
+      rm ${ora:0:8}plzln_s.dat
+      rm ${ora:0:8}plzln_g.dat
     done
     # inserire update DBMETEO
     SECONDS=0
